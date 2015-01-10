ed9d1a65d990416bfa7386273a4e164f5ec6810f
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2015-01-10 12:51:49 +0200

    vat calculations and sum count for each item in cart

--- cart.js
+++ cart.js
@@ -2,21 +2,35 @@
   var app = angular.module('webstore-cart',[]);
 
   app.controller('CartController', function(){
-    this.products = new Array(0);
+    this.cartItems = new Array(0);
+
     this.addProduct = function(product){
-      if(this.products.indexOf(product)===-1) {
-        this.products.push(product);
+      var productIndex = getItemCartIndex(this.cartItems, product);
+      if (productIndex===undefined){
+        this.cartItems.push({product: product, count: 1});
       } else {
-        product.count++;
+        this.cartItems[productIndex].count++;
       }
     };
+
     this.removeProduct = function(product){
-      if(product.count>1){
-        product.count--;
-      } else {
-        this.products.splice([this.products.indexOf(product)],1);
+      var productIndex = getItemCartIndex(this.cartItems, product);
+      if (productIndex!==undefined){
+        if (this.cartItems[productIndex].count===1){
+          this.cartItems.splice(productIndex,1);
+        } else {
+          this.cartItems[productIndex].count--;
+        }
       }
     };
+
+    function getItemCartIndex(cartItems, product){
+      for(var i=0; i < cartItems.length; i++){
+        if (cartItems[i].product===product){
+          return i;
+        }
+      }
+    }
   });
 
 })();
